74b0f098d720e7acd72148c3f320b4d5aed26697
NaeemUllah98 <naeem66241@gmail.com>
2025-11-15 22:09:42 +0500

    working on backend day before start

--- server/Server.js
+++ server/Server.js
@@ -1,17 +1,28 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res)=>{
+//     res.status(200).send("hello welcome in mern world");
+
+// })
+
+// app.get("/register", (req, res)=>{
+//     res.status(200).send("hello this is my 2nd api Naeem, now you can move to backend" )
+
+// })
+
+// const port= 5000;
+// app.listen(port, ()=>{
+//     console.log("my server is running");
+// })
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.status(200).send("hello welcome in mern world");
-
-})
-
-app.get("/register", (req, res)=>{
-    res.status(200).send("hello this is my 2nd api Naeem, now you can move to backend" )
-
+res.status(200).send("hello, his is my first api");
 })
 
 const port= 5000;
 app.listen(port, ()=>{
-    console.log("my server is running");
+    console.log("server running");
 })
